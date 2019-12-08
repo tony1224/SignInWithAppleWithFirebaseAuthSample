c24d2cdc9d8b18cbf6ec11c485b44db2f106a6de
Morita Jun <moritajun@MoritanoMacBook-Pro.local>
2019-12-08 15:22:17 +0900

    :up: LoginServiceImplの修正

---
+++ SignInWithAppleWithFirebaesAuthSample/UserAccount/Error/AuthenticationError.swift
@@ -0,0 +1,13 @@
+//
+//  AuthenticationError.swift
+//  UserAccount
+//
+//  Created by Morita Jun on 2019/12/10.
+//  Copyright © 2019 Morita Jun. All rights reserved.
+//
+
+import Foundation
+
+enum AuthenticationError: Error {
+    case failedToCreateCredential
+}
